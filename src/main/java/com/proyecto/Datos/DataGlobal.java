43d1923b4ccdb49cc3f2ddb0131770e2df74594d
Jose Rosales <167634761+Polaryys@users.noreply.github.com>
2025-10-22 20:43:16 -0600

    DataGlobal y Clases Principales

--- src/main/java/com/proyecto/Datos/DataGlobal.java
+++ src/main/java/com/proyecto/Datos/DataGlobal.java
@@ -4,9 +4,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataGlobal {
-    // Lista para almacenar todas las tareas
-    public static List<Tarea> listaTareas = new ArrayList<>();
 
-    // Lista para almacenar todos los hábitos
-    public static List<Habito> listaHabitos = new ArrayList<>();
 }
