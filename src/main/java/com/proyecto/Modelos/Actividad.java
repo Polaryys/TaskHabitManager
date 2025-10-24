2f93fdb2d177acba1fcf2a407f26037db0b4af26
Edin Mejía <edinmejia1208@gmail.com>
2025-10-24 17:30:08 -0600

    New update 1

--- src/main/java/com/proyecto/Modelos/Actividad.java
+++ src/main/java/com/proyecto/Modelos/Actividad.java
@@ -32,4 +32,25 @@ public class Actividad {
 
     public LocalTime getHora() { return hora; }
     public void setHora(LocalTime hora) { this.hora = hora; }
+
+    public static void mostrarReporte() {
+        LocalDate hoy = LocalDate.now();
+        LocalDate inicioSemana = hoy.minusDays(hoy.getDayOfWeek().getValue() - 1); 
+        LocalDate finSemana = inicioSemana.plusDays(6);
+        
+        System.out.println("estructura del reporte semanal ");
+        System.out.println("---------------------------------------");
+        System.out.println("periodo: " + inicioSemana + "al" + finSemana);
+        System.out.println();
+        System.out.println(" partes del reporte completo");
+        System.out.println(" perido semanal calculando en proceso.....");
+        System.out.println(" lista de tareas con prioridad");
+        System.out.println(" lista de habitos con frecuencia");
+        System.out.println(" porcentaje de actividades completadas");
+        System.out.println(" filtro por fechas");
+        System.out.println();
+        System.out.println("mas adelate se integrara con el gestor posteriormete");
+
+
+    }
 }
---
+++ src/main/java/com/proyecto/Modelos/pruebaAvance.java
@@ -0,0 +1,11 @@
+package main.java.com.proyecto.Modelos;
+
+public class pruebaAvance {
+    public static void main(String[] args) {
+        System.out.println("prueba del reporte semanal en consola");
+
+        Actividad.mostrarReporte();
+
+        System.out.println("avance en proceso");
+    }
+}
