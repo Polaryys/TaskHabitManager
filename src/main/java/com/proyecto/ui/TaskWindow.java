8f6db6feccbb5528f8038c6b649639f071e8b78c
Jose Rosales <167634761+Polaryys@users.noreply.github.com>
2025-10-26 13:42:46 -0600

    Cambios Nuevos

---
+++ src/main/java/com/proyecto/ui/TaskWindow.java
@@ -0,0 +1,67 @@
+package main.java.com.proyecto.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import main.java.com.proyecto.Modelos.*;
+import main.java.com.proyecto.Gestor.*;
+
+
+public class TaskWindow extends JDialog{
+    private Gestor gestor;
+    public TaskWindow(JFrame parent, Gestor gestor2 ){
+        super(parent, "Nueva  Tarea", true);
+        this.gestor = gestor2; // guardar la referencia
+        setSize(400, 250);
+        setLayout(new GridLayout(4, 2, 5, 5));
+        setLocationRelativeTo(parent);
+
+        JLabel TaskName = new JLabel("Nombre:");
+        JTextField txtName = new JTextField();
+
+        JLabel TaskDesc = new JLabel("Descripción:");
+        JTextField txtDesc = new JTextField();
+
+        JLabel TaskPriority = new JLabel("Prioridad (BAJA, MEDIA, ALTA):");
+        JTextField txtPriority = new JTextField();
+
+        JLabel TaskDate = new JLabel("Fecha (AAAA-MM-DD):");
+        JTextField txtDate = new JTextField();
+
+        JLabel TaskHour = new JLabel("Hora (HH:mm):");
+        JTextField txtHour = new JTextField();
+
+        JButton Guardar = new JButton("Guardar");
+        JButton Cancelar = new JButton("Cancelar");
+
+        add(TaskName); add(txtName);
+        add(TaskDesc); add(txtDesc);
+        add(TaskPriority); add(txtPriority);
+        add(TaskDate); add(txtDate);
+        add(TaskHour); add(txtHour);
+        add(Guardar); add(Cancelar);
+        
+        Guardar.addActionListener(e ->{
+            try{
+                String nombre = txtName.getText();
+                String descripcion = txtDesc.getText();
+                Tarea.Priority prioridad = Tarea.Priority.valueOf(txtPriority.getText().toUpperCase());
+                LocalDate fecha = LocalDate.parse(txtDate.getText());
+                LocalTime hora = LocalTime.parse(txtHour.getText());
+                
+                gestor.createTask(nombre, descripcion, prioridad, fecha, hora);
+
+                JOptionPane.showMessageDialog(this, "tarea Creada");
+                dispose();
+
+            } catch (Exception ex){
+                JOptionPane.showMessageDialog(this, "Error:" + ex.getMessage());
+            }
+        });
+        
+        Cancelar.addActionListener(e -> dispose());
+
+    }
+}
