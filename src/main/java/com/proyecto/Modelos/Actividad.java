1aa98cc463b0dc52ac9b1b6525a1152390770318
Jose Rosales <167634761+Polaryys@users.noreply.github.com>
2025-10-26 12:09:55 -0600

    DATOS ENUM

--- src/main/java/com/proyecto/Modelos/Actividad.java
+++ src/main/java/com/proyecto/Modelos/Actividad.java
@@ -4,13 +4,18 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Actividad {
+
+    public enum State{
+        COMPLETADO,
+        PENDIENTE
+    }
     private int id;
     private String nombre;
-    private String estado;
+    private State estado;
     private LocalDate fecha;
     private LocalTime hora;
 
-    public Actividad(int id, String nombre, String estado, LocalDate fecha, LocalTime hora) {
+    public Actividad(int id, String nombre, State estado, LocalDate fecha, LocalTime hora) {
         this.id = id;
         this.nombre = nombre;
         this.estado = estado;
@@ -24,8 +29,8 @@ public class Actividad {
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public String getEstado() { return estado; }
-    public void setEstado(String estado) { this.estado = estado; }
+    public State getEstado() { return estado; }
+    public void setEstado(State estado) { this.estado = estado; }
 
     public LocalDate getFecha() { return fecha; }
     public void setFecha(LocalDate fecha) { this.fecha = fecha; }
--- src/main/java/com/proyecto/Modelos/Habito.java
+++ src/main/java/com/proyecto/Modelos/Habito.java
@@ -4,14 +4,19 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Habito extends Actividad {
-    private String frecuencia;
+    public enum Frequency{
+        DIARIO,
+        SEMANAL,
+        MENSUAL
+    }
+    private Frequency frecuencia;
 
-    public Habito(int id, String nombre, String estado, LocalDate fecha, LocalTime hora,
-                  String frecuencia) {
+    public Habito(int id, String nombre, State estado, LocalDate fecha, LocalTime hora,
+                  Frequency frecuencia) {
         super(id, nombre, estado, fecha, hora);
         this.frecuencia = frecuencia;
     }
 
-    public String getFrecuencia() { return frecuencia; }
-    public void setFrecuencia(String frecuencia) { this.frecuencia = frecuencia; }
+    public Frequency getFrecuencia() { return frecuencia; }
+    public void setFrecuencia(Frequency frecuencia) { this.frecuencia = frecuencia; }
 }
