ba126e363f042917f441977b62a0029e099e02f9
Jose Rosales <167634761+Polaryys@users.noreply.github.com>
2025-10-27 22:04:45 -0600

    Creación de Tareas Completada

--- src/main/java/com/proyecto/Datos/DataGlobal.java
+++ src/main/java/com/proyecto/Datos/DataGlobal.java
@@ -1,8 +1,28 @@
 package main.java.com.proyecto.Datos;
 import main.java.com.proyecto.Modelos.*; 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 
 public class DataGlobal {
+    private static final String FILE_PATH = "Datos/Tareas.csv";
 
+    public void guardarTarea(Tarea tarea) {
+        try {
+            File folder = new File("Datos");
+            if (!folder.exists()) folder.mkdir();
+
+            FileWriter writer = new FileWriter(FILE_PATH, true);
+            writer.write(String.format("%d,%s,%s,%s,%s,%s,%s%n",
+                    tarea.getId(),
+                    tarea.getNombre(),
+                    tarea.getDescripcion(),
+                    tarea.getPrioridad(),
+                    tarea.getEstado(),
+                    tarea.getHora(),
+                    tarea.getFecha()
+                    ));
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Error al guardar la tarea: " + e.getMessage());
+        }
+    }
 }
