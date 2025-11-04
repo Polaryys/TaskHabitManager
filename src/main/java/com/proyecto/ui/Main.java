7aa10135f68651ac619041743159b2b3eeffdaef
Edin Mejía <edinmejia1208@gmail.com>
2025-11-03 18:52:50 -0600

    Arreglos que causaban bug

--- src/main/java/com/proyecto/ui/Main.java
+++ src/main/java/com/proyecto/ui/Main.java
@@ -35,6 +35,9 @@ public class Main extends JFrame {
         JButton DeleteActivity = new JButton("Eliminar Actividad");
         styleButton(DeleteActivity, buttonFont, new Color(159, 37, 6), textColor);
 
+        JButton Report = new JButton("Reporte semanal");
+        styleButton(Report, buttonFont, new Color(128, 0, 128), textColor);
+
         JPanel fila1 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         fila1.add(NewTask);
         fila1.add(NewHabit);
@@ -42,6 +45,7 @@ public class Main extends JFrame {
         JPanel fila2 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         fila2.add(EditActivity);
         fila2.add(DeleteActivity);
+        fila2.add(Report);
 
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
@@ -69,6 +73,12 @@ public class Main extends JFrame {
             DeleteWindow dialog = new DeleteWindow(this, gestor);
             dialog.setVisible(true);
         });
+
+        Report.addActionListener(e -> {
+            ReportWindow dialog = new ReportWindow(this, gestor);
+            dialog.setVisible(true);
+        }
+        );
     }
 
     private void styleButton(JButton button, Font font, Color bg, Color fg) {
