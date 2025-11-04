740961072ac8848eb4831c1b4cf29a957fd78b3d
Edin Mejía <edinmejia1208@gmail.com>
2025-11-03 18:29:26 -0600

    nuevos cambios

---
+++ src/main/java/com/proyecto/ui/ReportWindow.java
@@ -0,0 +1,31 @@
+package main.java.com.proyecto.ui;
+
+import main.java.com.proyecto.Gestor.Gestor;
+import main.java.com.proyecto.Modelos.Tarea;
+import main.java.com.proyecto.Modelos.Habito;
+
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class ReportWindow extends JDialog {
+    private Gestor gestor;
+
+     public ReportWindow(JFrame parent, Gestor gestor) {
+        super(parent, "Reporte Semanal", true);
+        this.gestor = gestor;
+
+        setSize(800, 600);
+        setLocationRelativeTo(parent);
+        setLayout(new BorderLayout(10, 10));
+
+    }
+
+    private String generarReporteSemanal() {
+
+        return "";
+    }
+    
+}
